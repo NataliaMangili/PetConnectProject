29d22aa502c242992113810b2efd1d94b679cabe
Natalia.Mangili <nataliaperico89@gmail.com>
2025-05-29 22:13:07 -0300

    feat: IdentityCore/Domain

---
+++ PetConnect/src/Identity/IdentityCore/Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using IdentityCore.Enums;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityCore.Entities;
+
+public class ApplicationUser : IdentityUser
+{
+    public string FullName { get; set; }
+    public UserType UserType { get; set; }
+    public string? CNPJ { get; set; }
+    public string? CPF { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
---
+++ PetConnect/src/Identity/IdentityCore/Domain/Entities/Enums/UserTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityCore.Enums;
+
+public enum UserType
+{
+    ONG = 1,
+    Protector = 2,
+    User = 3
+}
