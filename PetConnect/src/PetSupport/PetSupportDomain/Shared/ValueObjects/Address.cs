93b723d1d7821e649fc23b7614e9fecc292abd43
Natalia.Mangili <nataliaperico89@gmail.com>
2025-06-03 21:11:55 -0300

    feat: PetSupportDomain/VO

---
+++ PetConnect/src/PetSupport/PetSupportDomain/Shared/ValueObjects/Address.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetSupportDomain.Shared.ValueObjects;
+
+//DEIXAR NO SHARED??
+public record Address(string Street, string City, string State, string ZipCode, string Country);
+
---
+++ PetConnect/src/PetSupport/PetSupportDomain/Shared/ValueObjects/ContactInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetSupportDomain.Shared.ValueObjects;
+
+//DEIXAR NO SHARED??
+public record ContactInfo(string Phone, string Email);
+
