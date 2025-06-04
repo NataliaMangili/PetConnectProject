f5d69447eba04159ed14af421cba02e3dbe4383d
Natalia.Mangili <nataliaperico89@gmail.com>
2025-06-03 21:11:32 -0300

    feat: PetSupportInfra/DI

---
+++ PetConnect/src/PetSupport/PetSupportInfra/DependencyInjection/InfrastructureServiceCollection.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using PetSupportInfra.Persistence.Repositories.Base;
+using PetSupportInfra.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PetSupportInfra.Persistence.Context;
+using Microsoft.Extensions.Configuration;
+using PetSupportInfra.Persistence.Repositories.Adoption;
+
+namespace PetSupportInfra.DependencyInjection;
+
+public static class InfrastructureServiceCollection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<MongoDbContext>();
+        services.AddSingleton<IMongoClient>(sp =>
+        {
+            var settings = MongoClientSettings.FromConnectionString(configuration.GetConnectionString("MongoConnection"));
+            return new MongoClient(settings);
+        });
+
+        // Repositórios base
+        services.AddScoped(typeof(IBaseQueryRepository<>), typeof(IQueryRepository<>));
+
+
+        services.AddScoped<AdoptionCommandRepository>();
+        services.AddScoped<RescueRepository>();
+        services.AddScoped<LostPetRepository>();
+
+        return services;
+    }
+}
