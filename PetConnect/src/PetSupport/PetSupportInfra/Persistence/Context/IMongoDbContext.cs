5e80d9beef408b001c4bddbeeff6e6d2924b8f22
Natalia.Mangili <nataliaperico89@gmail.com>
2025-06-03 21:11:09 -0300

    feat: PetSupportInfra/MongoDb

---
+++ PetConnect/src/PetSupport/PetSupportInfra/Persistence/Context/IMongoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetSupportInfra.Persistence.Context;
+
+public interface IMongoDbContext
+{
+    IMongoCollection<T> GetCollection<T>(string collectionName);
+}
---
+++ PetConnect/src/PetSupport/PetSupportInfra/Persistence/Context/MongoDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetSupportInfra.Persistence.Context;
+
+public class MongoDbContext : IMongoDbContext
+{
+    private readonly IMongoDatabase _database;
+
+    public MongoDbContext(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration.GetConnectionString("MongoConnection"));
+        _database = client.GetDatabase(configuration["MongoDatabase"]);
+    }
+
+    public IMongoCollection<T> GetCollection<T>(string name) =>
+        _database.GetCollection<T>(name);
+
+}
