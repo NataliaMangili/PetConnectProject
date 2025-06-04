dd20c0e6e70299d2319284829311f68d63ffb436
Natalia.Mangili <nataliaperico89@gmail.com>
2025-06-03 21:10:42 -0300

    feat: PetSupportInfra/Repos

---
+++ PetConnect/src/PetSupport/PetSupportInfra/Persistence/Repositories/Adoption/AdoptionCommandRepository.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Driver;
+using PetSupportInfra.Persistence.Context;
+using PetSupportInfra.Persistence.Repositories.Base;
+using PetSupportDomain.Adoption.Models;
+
+namespace PetSupportInfra.Persistence.Repositories.Adoption;
+
+public class AdoptionCommandRepository : IBaseCommandRepository<AdoptionPet>
+{
+    private readonly IMongoCollection<AdoptionPet> _collection;
+
+    public AdoptionCommandRepository(MongoDbContext context)
+    {
+        _collection = context.GetCollection<AdoptionPet>("Adoptions");
+    }
+
+    public async Task AddAsync(AdoptionPet entity)
+    {
+        await _collection.InsertOneAsync(entity);
+    }
+
+    public async Task UpdateAsync(string id, AdoptionPet entity)
+    {
+        await _collection.ReplaceOneAsync(x => x.Id == id, entity);
+    }
+
+    public async Task DeleteAsync(string id)
+    {
+        await _collection.DeleteOneAsync(x => x.Id == id);
+    }
+}
---
+++ PetConnect/src/PetSupport/PetSupportInfra/Persistence/Repositories/Adoption/AdoptionQueryRepository.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Driver;
+using PetSupportDomain.Adoption.Models;
+using PetSupportInfra.Persistence.Context;
+using PetSupportInfra.Persistence.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetSupportInfra.Persistence.Repositories.Adoption;
+
+public class AdoptionQueryRepository : IBaseQueryRepository<AdoptionPet>
+{
+    private readonly IMongoCollection<AdoptionPet> _collection;
+
+    public AdoptionQueryRepository(MongoDbContext context)
+    {
+        _collection = context.GetCollection<AdoptionPet>("Adoptions");
+    }
+
+    public async Task<AdoptionPet> GetByIdAsync(string id)
+    {
+        return (await _collection.FindAsync(x => x.Id == id)).FirstOrDefault();
+    }
+
+    public async Task<IEnumerable<AdoptionPet>> GetAllAsync()
+    {
+        return (await _collection.FindAsync(_ => true)).ToList();
+    }
+
+    public async Task<IEnumerable<AdoptionPet>> FindAsync(Expression<Func<AdoptionPet, bool>> filter)
+    {
+        return (await _collection.FindAsync(filter)).ToList();
+    }
+}
+
