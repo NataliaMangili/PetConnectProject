e82206488813f852280a01c161fe1f1a55b4424a
Natalia.Mangili <nataliaperico89@gmail.com>
2025-06-04 21:48:24 -0300

    feat: PetSupportDomain/Shared/Interfaces

---
+++ PetConnect/src/PetSupport/PetSupportDomain/Shared/Interfaces/IAggregateRoot.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetSupportDomain.Shared.Interfaces;
+
+public interface IAggregateRoot : IEntity { }
