ea28261687d5901fabd2ba8ce5ae0daeb1ab7324
Natalia.Mangili <nataliaperico89@gmail.com>
2025-06-05 20:40:15 -0300

    feat: PetSupportDomain/Shared and Events

---
+++ PetConnect/src/PetSupport/PetSupportDomain/Rescue/Events/PetRescuedEvent.cs
@@ -0,0 +1,24 @@
+﻿using PetSupportDomain.Shared.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetSupportDomain.Rescue.Events;
+
+public class PetRescuedEvent : IEvent
+{
+    public string EventId { get; } = Guid.NewGuid().ToString();
+    public string AggregateId { get; }
+    public DateTime OccurredOn { get; } = DateTime.UtcNow;
+    public string CorrelationId { get; }
+    public string RescuerName { get; }
+
+    public PetRescuedEvent(string aggregateId, string rescuerName, string correlationId)
+    {
+        AggregateId = aggregateId;
+        RescuerName = rescuerName;
+        CorrelationId = correlationId;
+    }
+}
---
+++ PetConnect/src/PetSupport/PetSupportDomain/Shared/Aggregates/PetAggregate.cs
@@ -0,0 +1,34 @@
+﻿using PetSupportDomain.Adoption.Events;
+using PetSupportDomain.Shared.Events;
+
+namespace PetSupportDomain.Shared.Aggregates;
+
+public class PetAggregate
+{
+    public string Id { get; private set; }
+    public string Status { get; private set; }
+
+    private readonly List<IEvent> _events = new();
+    public IReadOnlyCollection<IEvent> Events => _events;
+
+    public PetAggregate(string id, string status = "Available")
+    {
+        Id = id;
+        Status = status;
+    }
+
+    public void RequestAdoption(string userId, string correlationId)
+    {
+        if (Status != "Available")
+            throw new InvalidOperationException("Pet is not available for adoption.");
+
+        var adoptionEvent = new AdoptionRequestedEvent("a", Id, userId, correlationId);
+        Apply(adoptionEvent);
+        _events.Add(adoptionEvent);
+    }
+
+    public void Apply(AdoptionRequestedEvent evt)
+    {
+        Status = "AdoptionRequested";
+    }
+}
