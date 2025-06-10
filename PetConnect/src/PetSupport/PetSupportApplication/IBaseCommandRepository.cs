99d2d3a31bec6939ffd5de38d1418624007588aa
Natalia.Mangili <nataliaperico89@gmail.com>
2025-06-09 21:52:04 -0300

    feat: PetSupport/Application/Base

---
+++ PetConnect/src/PetSupport/PetSupportApplication/IBaseCommandRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace PetSupportApplication;
+
+public interface IBaseCommandRepository<T> where T : class
+{
+    Task<T> InsertAsync(T entity);
+    Task UpdateAsync(string id, T entity);
+    Task DeleteAsync(string id);
+}
---
+++ PetConnect/src/PetSupport/PetSupportApplication/IBaseQueryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetSupportApplication;
+
+public interface IBaseQueryRepository<T> where T : class
+{
+    Task<T> GetByIdAsync(string id);
+    Task<List<T>> GetAllAsync();
+    Task<List<T>> FilterAsync(Expression<Func<T, bool>> filter);
+}
