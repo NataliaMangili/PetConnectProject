711cac1d7cf12fe012c242480f9a1bb0c5694200
Natalia.Mangili <nataliaperico89@gmail.com>
2025-06-09 21:51:30 -0300

    feat: PetSupport/Adoption/Commands

---
+++ PetConnect/src/PetSupport/PetSupportApplication/Adoption/Commands/CreateAdoption/CreateAdoptionCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetSupportApplication.Adoption.Commands.CreateAdoption;
+
+public class CreateAdoptionCommand : IRequest<string>
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
---
+++ PetConnect/src/PetSupport/PetSupportApplication/Adoption/Commands/CreateAdoption/CreateAdoptionCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using PetSupportDomain.Adoption.Interfaces;
+using PetSupportDomain.Adoption.Models;
+using PetSupportDomain.Shared.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetSupportApplication.Adoption.Commands.CreateAdoption;
+
+public class CreateAdoptionCommandHandler : IRequestHandler<CreateAdoptionCommand, string>
+{
+    private readonly IAdoptionCommandRepository _repository;
+
+    public CreateAdoptionCommandHandler(IAdoptionCommandRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<string> Handle(CreateAdoptionCommand request, CancellationToken cancellationToken)
+    {
+        var adoption = new AdoptionPetAggregate(
+            id: Guid.NewGuid().ToString()
+            //petId: request.PetId,
+            //adopterName: request.AdopterName,
+            //contactInfo: request.ContactInfo,
+            //adoptionDate: DateTime.UtcNow
+        );
+
+        await _repository.InsertAsync(adoption);
+
+        return adoption.Id;
+    }
+}
