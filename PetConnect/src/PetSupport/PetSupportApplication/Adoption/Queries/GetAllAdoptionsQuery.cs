86e7c8ba6d1bdfe1b742544208b5067f97648d35
Natalia.Mangili <nataliaperico89@gmail.com>
2025-06-09 21:51:18 -0300

    feat: PetSupport/Adoption/Queries

---
+++ PetConnect/src/PetSupport/PetSupportApplication/Adoption/Queries/GetAllAdoptionsQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using PetSupportDomain.Adoption.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetSupportApplication.Adoption.Queries;
+
+public class GetAllAdoptionsQuery : IRequest<List<AdoptionPetAggregate>>
+{
+}
---
+++ PetConnect/src/PetSupport/PetSupportApplication/Adoption/Queries/GetAllAdoptionsQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using PetSupportDomain.Adoption.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetSupportApplication.Adoption.Queries;
+
+public class GetAllAdoptionsQueryHandler : IRequestHandler<GetAllAdoptionsQuery, List<AdoptionPetAggregate>>
+{
+    private readonly IBaseQueryRepository<AdoptionPetAggregate> _repository;
+
+    public GetAllAdoptionsQueryHandler(IBaseQueryRepository<AdoptionPetAggregate> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<List<AdoptionPetAggregate>> Handle(GetAllAdoptionsQuery request, CancellationToken cancellationToken)
+    {
+        return await _repository.GetAllAsync();
+    }
+}
