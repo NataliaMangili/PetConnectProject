c44767857fa5482fa16ebdb064ee224806914108
Natalia.Mangili <nataliaperico89@gmail.com>
2025-06-27 20:02:01 -0300

    feat: appsettings, csproj, program

--- PetConnect/src/PetSupport/PetSupportAPI/Program.cs
+++ PetConnect/src/PetSupport/PetSupportAPI/Program.cs
@@ -1,3 +1,7 @@
+using PetSupportApplication;
+using PetSupportApplication.Adoption.Commands.CreateAdoption;
+using PetSupportInfra.Persistence.Repositories.Base;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssembly(typeof(CreateAdoptionCommandHandler).Assembly));
+
+builder.Services.AddScoped(typeof(IBaseCommandRepository<>), typeof(BaseCommandRepository<>));
+builder.Services.AddScoped(typeof(IBaseQueryRepository<>), typeof(BaseQueryRepository<>));
+
+
 
 var app = builder.Build();
 
