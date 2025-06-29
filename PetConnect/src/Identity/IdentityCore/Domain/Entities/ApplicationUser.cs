156fb7d64c5e60efedeaf7cda5b76a946e549c28
Natalia.Mangili <nataliaperico89@gmail.com>
2025-06-29 17:48:33 -0300

    Fix: IdentityApiCore

--- PetConnect/src/Identity/IdentityCore/Domain/Entities/ApplicationUser.cs
+++ PetConnect/src/Identity/IdentityCore/Domain/Entities/ApplicationUser.cs
@@ -1,18 +1,39 @@
-﻿using IdentityCore.Domain.Entities.Enums;
-using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Identity;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IdentityCore.Domain.Entities;
 
-public class ApplicationUser : IdentityUser
+public abstract class ApplicationUser : IdentityUser<Guid>
 {
-    public string FullName { get; set; }
-    public UserType UserType { get; set; }
-    public string? CNPJ { get; set; }
-    public string? CPF { get; set; }
-    public DateTime CreatedAt { get; set; }
+    public DateTime CreatedAt { get; protected set; }
+
+    protected ApplicationUser()
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    protected ApplicationUser(string email) : this()
+    {
+        SetEmail(email);
+    }
+
+    public void SetEmail(string email)
+    {
+        if (string.IsNullOrWhiteSpace(email) || !email.Contains("@"))
+            throw new ArgumentException("E-mail inválido.");
+
+        Email = email.ToLower();
+        UserName = Email;
+        NormalizedEmail = email.ToUpperInvariant();
+        NormalizedUserName = UserName.ToUpperInvariant();
+    }
+
+    public void SetPasswordHash(string hash)
+    {
+        if (string.IsNullOrWhiteSpace(hash))
+            throw new ArgumentException("Hash de senha inválido.");
+        PasswordHash = hash;
+    }
 }
