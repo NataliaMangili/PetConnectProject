a99037f50eecc45f58feb183b45eaeaba29c867b
Natalia.Mangili <nataliaperico89@gmail.com>
2025-06-29 17:47:02 -0300

    fix: IdentityInfra

---
+++ PetConnect/src/Identity/IdentityInfra/IdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using IdentityCore.Domain.Entities;
+
+namespace IdentityInfra;
+
+public class IdentityDbContext : DbContext
+{
+    public IdentityDbContext(DbContextOptions<IdentityDbContext> options)
+        : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Ong> Ongs { get; set; }
+    public DbSet<Protector> Protectors { get; set; }
+}
---
+++ PetConnect/src/Identity/IdentityInfra/Repositories/BaseRepository.cs
@@ -0,0 +1,49 @@
+﻿using IdentityCore.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity.Migrations;
+
+namespace IdentityInfra.Repositories;
+
+public class BaseRepository<T> : IRepository<T> where T : class
+{
+    protected readonly IdentityDbContext _context;
+    protected readonly DbSet<T> _dbSet;
+
+    public BaseRepository(IdentityDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+    }
+
+    public async Task AddAsync(T entity)
+    {
+        _dbSet.Add(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<T> GetByIdAsync(string id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        _dbSet.Update(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(string id)
+    {
+        var entity = await GetByIdAsync(id);
+        if (entity != null)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
