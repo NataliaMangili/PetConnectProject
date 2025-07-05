2c5466ec52340763b12c968798ebc3c2cfcd2de5
Natalia.Mangili <nataliaperico89@gmail.com>
2025-07-04 21:02:16 -0300

    feat: MarketplaceAPI/Repos

---
+++ PetConnect/src/Marketplace/MarketplaceAPI/Infra/Repositories/Repository.cs
@@ -0,0 +1,44 @@
+﻿using MarketplaceAPI.Infra.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace MarketplaceAPI.Infra.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    protected readonly MarketplaceDbContext _context;
+    protected readonly DbSet<T> _dbSet;
+
+    public Repository(MarketplaceDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+    public virtual async Task<T> GetByIdAsync(Guid id) =>
+        await _dbSet.FindAsync(id);
+
+    public virtual async Task<IEnumerable<T>> GetAllAsync() =>
+        await _dbSet.ToListAsync();
+
+    public virtual async Task AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public virtual async Task UpdateAsync(T entity)
+    {
+        _dbSet.Update(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public virtual async Task DeleteAsync(Guid id)
+    {
+        var entity = await GetByIdAsync(id);
+        if (entity == null) return;
+
+        _dbSet.Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+}
