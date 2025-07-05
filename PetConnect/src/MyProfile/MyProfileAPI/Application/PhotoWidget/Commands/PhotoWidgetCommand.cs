5755db490c5f47675f94293f90f7d735f8db2ec8
Natalia.Mangili <nataliaperico89@gmail.com>
2025-07-05 18:59:57 -0300

    feat: MyProfileAPI/Application/PhotoWidget/CommandQuerys

---
+++ PetConnect/src/MyProfile/MyProfileAPI/Application/PhotoWidget/Commands/PhotoWidgetCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace MyProfileAPI.Application.PhotoWidget.Commands;
+
+public record CreatePhotoWidgetCommand(Guid OngId, string Url, string Description) : IRequest<Guid>;
+
---
+++ PetConnect/src/MyProfile/MyProfileAPI/Application/PhotoWidget/Querys/GetPhotosByOngQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using MyProfileAPI.Application.PhotoWidget.DTOs;
+
+namespace MyProfileAPI.Application.PhotoWidget.Querys;
+
+public record GetPhotosByOngQuery(Guid OngId) : IRequest<IEnumerable<PhotoWidgetDto>>;
